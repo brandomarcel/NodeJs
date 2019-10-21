90fda085b896ab3038979ce3b4be9cca3e53301c
Brandonmarc <brandocevallos@gmail.com>
2019-10-20 23:13:00 -0500

    version 1

---
+++ EJEMPLONODE/index.js
@@ -0,0 +1,11 @@
+const math = require('./FuncionesMatematicas.js');
+
+console.log(math.multiplicar(5,5));
+
+
+console.log(math.restar(5,5));
+
+
+console.log(math.sumar(5,5));
+
+console.log(math.dividir(5,5));
